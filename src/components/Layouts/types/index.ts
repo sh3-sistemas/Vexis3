94dbfac1e5357cb8b2ff070b9089f59c548790d1
guilherme-batista-sh3 <guilherme.evangelista@sh3.com.br>
2024-11-29 15:17:21 -0300

    feature(components): Importação de component/Layouts

---
+++ src/components/Layouts/types/index.ts
@@ -0,0 +1,70 @@
+/**
+ *
+ * Sh3LayoutMock is a Vue component.
+ *
+ * [Component](./Sh3LayoutMock.vue)
+ *
+ * @module layouts
+ *
+ */
+import type { DefineComponent } from "vue";
+import type { Sh3UserNavBarProps } from "@/components/Navigation";
+import type { Sh3ToggleSidebarProps, Sh3RouterLinkTabsProps } from "@/types";
+import type { AppBaseHeaderProps } from "../fragments/AppBaseHeader.vue";
+import type { AppBaseToolbarProps } from "../fragments/AppBaseToolbar.vue";
+/**
+ * **SH3 - Sh3LayoutMock**
+ *
+ * _Sh3LayoutMock is a Vue component._
+ * --- ---
+ * [Vue Based Component](./Sh3LayoutMock.vue)
+ * @group Component
+ *
+ */
+
+/**
+ * **SH3 - Sh3PageNotFound**
+ *
+ * _Sh3PageNotFound is a Vue component._
+ * --- ---
+ * [Vue Based Component](./Sh3PageNotFound.vue)
+ * @group Component
+ *
+ */
+
+/**
+ * **SH3 - Sh3Vertical**
+ *
+ * _Sh3Vertical is a Vue component._
+ * --- ---
+ * [Vue Based Component](./Sh3Vertical.vue)
+ * @group Component
+ *
+ */
+
+/**
+ * **SH3 - Sh3Vertical**
+ *
+ * _Sh3Vertical is a Vue component._
+ * --- ---
+ * [Vue Based Component](./Sh3Vertical.vue)
+ * @group Component
+ *
+ */
+
+export type AppBaseLayoutProps = {
+  navbar: Sh3UserNavBarProps;
+  side: Sh3ToggleSidebarProps;
+  toolbar: AppBaseToolbarProps & { hidden: boolean };
+  header: AppBaseHeaderProps;
+  tabs: Sh3RouterLinkTabsProps;
+};
+
+declare module "vue" {
+  export interface GlobalComponents {
+    Sh3Vertical: DefineComponent<any, any, any>;
+    Sh3LayoutMock: DefineComponent<any, any, any>;
+    Sh3PageNotFound: DefineComponent<any, any, any>;
+    Sh3AppBaseLayout: DefineComponent<AppBaseLayoutProps, any, any>;
+  }
+}
