0ff775a7d2a896ebfd69d5aa2b25639592815b83
guilherme-batista-sh3 <guilherme.evangelista@sh3.com.br>
2025-01-14 15:12:45 -0300

    Feature(component): Implementação de Sh3Tabs

--- src/components/Tabs/index.ts
+++ src/components/Tabs/index.ts
@@ -1,4 +1,5 @@
 import Sh3RouterLinkTabs from "./Sh3RouterLinkTabs.vue";
+import Sh3Tabs from "./Sh3Tabs.vue";
 
-export { Sh3RouterLinkTabs };
+export { Sh3RouterLinkTabs, Sh3Tabs };
 export * from "./types";
