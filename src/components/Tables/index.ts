021d69e946e3eb70cb37012e007a9b1c39bfa3b3
Bárbara Boechat <barbs.boechat@gmail.com>
2025-01-17 10:44:47 -0300

    feature(column): Export da coluna do primevue para uso na tabela em um slot custom

--- src/components/Tables/index.ts
+++ src/components/Tables/index.ts
@@ -2,5 +2,12 @@ import Sh3DataTable from "./DataTable/Sh3DataTable.vue";
 import Sh3DataTableEditable from "./DataTable/Sh3DataTableEditable.vue";
 import Sh3FilterTags from "./Filters/Sh3FilterTags.vue";
 import usePagination from "./composables/usePagination";
+import Column from "primevue/column";
 
-export { Sh3DataTable, Sh3DataTableEditable, Sh3FilterTags, usePagination };
+export {
+  Sh3DataTable,
+  Sh3DataTableEditable,
+  Sh3FilterTags,
+  Column,
+  usePagination,
+};
