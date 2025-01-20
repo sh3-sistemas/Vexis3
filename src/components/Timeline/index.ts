b708d54866d76a3dbd3b5644584cadf81d12d4a6
Mariana Castro <mariana.castro.sh3@gmail.com>
2025-01-20 17:16:06 -0300

    [FEATURE] inclusão do componente de timeline

---
+++ src/components/Timeline/index.ts
@@ -0,0 +1,3 @@
+import { Timeline } from "primevue";
+
+export { Timeline };
--- src/components/index.ts
+++ src/components/index.ts
@@ -15,4 +15,5 @@ export * from "./Sidebars";
 export * from "./Tables";
 export * from "./Tabs";
 export * from "./Tags";
+export * from "./Timeline";
 export * from "./Typography";
