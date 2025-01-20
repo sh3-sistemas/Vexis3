82cf38878c0b70744175d09da1e9e137cbe322b9
guilherme-batista-sh3 <guilherme.evangelista@sh3.com.br>
2025-01-20 14:38:45 -0300

    Feature(componente): Implementação de Sh3Competencia

--- src/components/Dropdowns/types/index.ts
+++ src/components/Dropdowns/types/index.ts
@@ -20,10 +20,47 @@ export interface Sh3HeaderSwitcher {
   "footer"(): VNode[];
 }
 
+/**
+ * **SH3 - Sh3Competencia**
+ *
+ * _Sh3Competencia is a Vue component._
+ * --- ---
+ * [Vue Based Component](./Sh3Competencia.vue)
+ * @group Component
+ *
+ */
+
+export type Competencia = {
+  mes: string;
+  estado: string;
+};
+
+export interface Sh3CompetenciaProps {
+  competencias: Competencia[];
+  value?: Competencia;
+}
+
+/**
+ * Defines valid emits in Sh3Competencia component.
+ */
+export interface Sh3CompetenciaEmits {
+  /**
+   * Callback to invoke when the value changes.
+   */
+  gereciar(): void;
+  selecionar(): Competencia;
+  newCompetencia(): void;
+  changeYear(): number;
+}
 declare module "vue" {
   export interface GlobalComponents {
     Sh3DropdownMenu: DefineComponent<any, any, any>;
     Sh3SelectDropdown: DefineComponent<any, any, any>;
     Sh3UserDropdown: DefineComponent<any, any, any>;
+    Sh3Competencia: DefineComponent<
+      Sh3CompetenciaProps,
+      any,
+      Sh3CompetenciaEmits
+    >;
   }
 }
