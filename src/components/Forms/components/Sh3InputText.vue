287c69860f39e0a4ae0fc7b5ed5a6d127db10685
Mariana Castro <mariana.castro.sh3@gmail.com>
2025-02-10 08:27:24 -0300

    [FEATURE] inclusão dos componentes de input básico (Sh3InputText) e de input com button (Sh3InputWithButton)

---
+++ src/components/Forms/components/Sh3InputText.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import InputText from "primevue/inputtext";
+
+defineOptions({
+  inheritAttrs: false,
+});
+</script>
+<template>
+  <InputText :pt-options="{ mergeProps: true }" v-bind="$attrs" />
+</template>
--- src/components/Forms/index.ts
+++ src/components/Forms/index.ts
@@ -3,6 +3,15 @@ import { Sh3Input, Sh3Checkbox } from "./Deprecated/Inputs";
 import Sh3Select from "./Deprecated/Sh3Select.vue";
 
 import Sh3GenericForm from "./Sh3GenericForm.vue";
+import Sh3InputWithButton from "./components/Sh3InputWithButton.vue";
 
-export { DynamicForm, Sh3Select, Sh3Input, Sh3Checkbox, Sh3GenericForm };
 export * from "./types";
+
+export {
+  DynamicForm,
+  Sh3Select,
+  Sh3Input,
+  Sh3Checkbox,
+  Sh3GenericForm,
+  Sh3InputWithButton,
+};
