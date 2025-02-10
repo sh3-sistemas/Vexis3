ab31385c1b0a9acfc0eaa31a9b0049f4be3e08a7
Mariana Castro <mariana.castro.sh3@gmail.com>
2025-02-10 08:27:24 -0300

    [FEATURE] inclusão dos componentes de input básico (Sh3InputText) e de input com button (Sh3InputWithButton)

---
+++ src/components/Forms/components/Sh3InputWithButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import InputGroup from "primevue/inputgroup";
+import Sh3Button from "@/components/Buttons/Sh3Button.vue";
+import Sh3InputText from "./Sh3InputText.vue";
+import type { Sh3InputWithButtonProps } from "./types";
+
+defineProps<Sh3InputWithButtonProps>();
+
+defineOptions({
+  inheritAttrs: false,
+});
+</script>
+<template>
+  <InputGroup :pt-options="{ mergeProps: true }" v-bind="$attrs">
+    <Sh3InputText v-bind="inputProps" />
+    <Sh3Button v-bind="buttonProps" />
+  </InputGroup>
+</template>
--- src/components/Forms/components/types/index.ts
+++ src/components/Forms/components/types/index.ts
@@ -1,4 +1,25 @@
+/**
+ * @module forms
+ */
 import type { ButtonElementProps } from "@vueform/vueform";
-import type { ButtonProps } from "primevue";
+import type { ButtonProps, InputTextEmits, InputTextProps } from "primevue";
+import type { DefineComponent } from "vue";
 
+/**
+ * Defines valid properties in FormButton component.
+ */
 export type Sh3FormButtonProps = ButtonElementProps & ButtonProps;
+
+/**
+ * Defines valid properties in InputWithButton component.
+ */
+export type Sh3InputWithButtonProps = {
+  inputProps: InputTextProps;
+  buttonProps: ButtonElementProps;
+};
+
+declare module "vue" {
+  export interface GlobalComponents {
+    Sh3InputText: DefineComponent<InputTextProps, {}, InputTextEmits>;
+  }
+}
