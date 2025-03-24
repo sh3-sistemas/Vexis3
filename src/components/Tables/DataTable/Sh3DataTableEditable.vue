23e336a14f8f9a2d12d212f4047d1ad6910c866a
Bárbara Boechat <barbs.boechat@gmail.com>
2025-03-24 09:02:40 -0300

    feat: avanços na configuração do primevue com tema Aura

--- src/components/Tables/DataTable/Sh3DataTableEditable.vue
+++ src/components/Tables/DataTable/Sh3DataTableEditable.vue
@@ -64,19 +64,19 @@
       <template #body="{ data: row, field }">
         <Checkbox
           v-if="col.filterType.toLowerCase() == 'boolean'"
-          v-model="row[field]"
+          v-model="row[field as keyof typeof row]"
           disabled
           :binary="true"
         />
         <div v-else-if="col.cellFormater">
           <component :is="col.cellFormater" v-bind="{ row, field }"></component>
         </div>
-        <div v-else>{{ getValueByPath(row, field) }}</div>
+        <div v-else>{{ getValueByPath(row, field as string) }}</div>
       </template>
       <template v-if="col.editable != false" #editor="{ data: row, field }">
         <Checkbox
           v-if="col.filterType.toLowerCase() == 'boolean'"
-          v-model="row[field]"
+          v-model="row[field as keyof typeof row]"
           :binary="true"
         />
         <div v-else-if="col.cellFormaterEdit">
