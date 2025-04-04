a934e67e77edd27e2e47777780e46b1e32007361
Mariana Castro <mariana.castro.sh3@gmail.com>
2025-04-04 11:16:31 -0300

    refactor(aura): correção do theme aura
    - Inclusão das cores padrões da sh3 como primitive tokens;
    - Remoção das cores padrões do tailwind anteriormente sobrescritas;
    - Correção das cores na customização do preset;
    - Inclusão de customizações específicas para os componentes, corrigindo
    as cores utilizadas para as padrões.

---
+++ src/theme/aura/components/inlinemessage.ts
@@ -0,0 +1,58 @@
+export default {
+  light: {
+    info: {
+      background: "color-mix(in srgb, {selenium.50}, transparent 5%)",
+      borderColor: "{selenium.200}",
+      color: "{selenium.600}",
+      shadow:
+        "0px 4px 8px 0px color-mix(in srgb, {selenium.500}, transparent 96%)",
+    },
+    success: {
+      background: "color-mix(in srgb, {jade.50}, transparent 5%)",
+      borderColor: "{jade.200}",
+      color: "{jade.600}",
+      shadow: "0px 4px 8px 0px color-mix(in srgb, {jade.500}, transparent 96%)",
+    },
+    warn: {
+      background: "color-mix(in srgb,{sulfur.50}, transparent 5%)",
+      borderColor: "{sulfur.200}",
+      color: "{sulfur.600}",
+      shadow:
+        "0px 4px 8px 0px color-mix(in srgb, {sulfur.500}, transparent 96%)",
+    },
+    error: {
+      background: "color-mix(in srgb, {ruby.50}, transparent 5%)",
+      borderColor: "{ruby.200}",
+      color: "{ruby.600}",
+      shadow: "0px 4px 8px 0px color-mix(in srgb, {ruby.500}, transparent 96%)",
+    },
+  },
+  dark: {
+    info: {
+      background: "color-mix(in srgb, {selenium.500}, transparent 84%)",
+      borderColor: "color-mix(in srgb, {selenium.700}, transparent 64%)",
+      color: "{selenium.500}",
+      shadow:
+        "0px 4px 8px 0px color-mix(in srgb, {selenium.500}, transparent 96%)",
+    },
+    success: {
+      background: "color-mix(in srgb, {jade.500}, transparent 84%)",
+      borderColor: "color-mix(in srgb, {jade.700}, transparent 64%)",
+      color: "{jade.500}",
+      shadow: "0px 4px 8px 0px color-mix(in srgb, {jade.500}, transparent 96%)",
+    },
+    warn: {
+      background: "color-mix(in srgb, {sulfur.500}, transparent 84%)",
+      borderColor: "color-mix(in srgb, {sulfur.700}, transparent 64%)",
+      color: "{sulfur.500}",
+      shadow:
+        "0px 4px 8px 0px color-mix(in srgb, {sulfur.500}, transparent 96%)",
+    },
+    error: {
+      background: "color-mix(in srgb, {ruby.500}, transparent 84%)",
+      borderColor: "color-mix(in srgb, {ruby.700}, transparent 64%)",
+      color: "{ruby.500}",
+      shadow: "0px 4px 8px 0px color-mix(in srgb, {ruby.500}, transparent 96%)",
+    },
+  },
+};
