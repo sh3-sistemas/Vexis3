1af0d3268c77c214d768dc26ab4c127dbbba2ef5
Mariana Castro <mariana.castro.sh3@gmail.com>
2025-04-04 11:16:31 -0300

    refactor(aura): correção do theme aura
    - Inclusão das cores padrões da sh3 como primitive tokens;
    - Remoção das cores padrões do tailwind anteriormente sobrescritas;
    - Correção das cores na customização do preset;
    - Inclusão de customizações específicas para os componentes, corrigindo
    as cores utilizadas para as padrões.

---
+++ src/theme/aura/components/progressspiner.ts
@@ -0,0 +1,18 @@
+export default {
+  light: {
+    root: {
+      colorOne: "{ruby.500}",
+      colorTwo: "{selenium.500}",
+      colorThree: "{jade.500}",
+      colorFour: "{sulfur.500}",
+    },
+  },
+  dark: {
+    root: {
+      colorOne: "{ruby.400}",
+      colorTwo: "{selenium.400}",
+      colorThree: "{jade.400}",
+      colorFour: "{sulfur.400}",
+    },
+  },
+};
