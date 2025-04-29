f9fbedc25530a2f4b4b21f22d694467f215f824b
Mariana Castro <mariana.castro.sh3@gmail.com>
2025-04-29 17:47:28 -0300

    refactor(loading): ajuste no componente de loading, tornando opcional a prop de styling

--- src/components/Layouts/Sh3Loading.vue
+++ src/components/Layouts/Sh3Loading.vue
@@ -3,12 +3,12 @@
     :class="
       twMerge(
         'text-center content-center rounded-xl bg-primary-50',
-        styling.container,
+        styling?.container,
       )
     "
   >
-    <i :class="twMerge('pi pi-spin pi-spinner', styling.icon)" />
-    <p :class="twMerge(styling.text)">
+    <i :class="twMerge('pi pi-spin pi-spinner', styling?.icon)" />
+    <p :class="twMerge(styling?.text)">
       {{ subText }}
     </p>
   </div>
@@ -17,7 +17,7 @@
 <script lang="ts" setup>
 import { twMerge } from "tailwind-merge";
 export type Sh3LoadingProps = {
-  styling: {
+  styling?: {
     container?: string;
     icon?: string;
     text?: string;
