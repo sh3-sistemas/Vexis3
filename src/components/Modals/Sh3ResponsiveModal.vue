793ee448a8b816d21a615a6640d522686747f43e
antonio-martins-sh3 <ac.martins@sh3.com>
2025-06-12 09:44:37 -0300

    techdebit(adicionar-breadcrumbs): correcao de PR

--- src/components/Modals/Sh3ResponsiveModal.vue
+++ src/components/Modals/Sh3ResponsiveModal.vue
@@ -5,14 +5,19 @@
     :breakpoints="{ '1199px': '75vw', '575px': 'w-full' }"
     v-bind="$attrs"
   >
-    <template #header>
-      <slot name="header"></slot>
+    <template
+      v-for="(slot, index) of slotNames"
+      :key="index"
+      #[slot]="slotProps"
+    >
+      <slot :name="slot" v-bind="{ ...(slotProps as object) }" />
     </template>
     <slot></slot>
   </Dialog>
 </template>
 
 <script lang="ts" setup>
+import { useSlots } from "vue";
 import Dialog from "primevue/dialog";
 
 const visible = defineModel<boolean>();
@@ -20,4 +25,8 @@ const visible = defineModel<boolean>();
 defineOptions({
   inheritAttrs: false,
 });
+
+const slots = useSlots();
+// Assert type here to prevent errors in template
+const slotNames = Object.keys(slots) as unknown;
 </script>
