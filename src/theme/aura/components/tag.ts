9efcbd66449ad2e09c426ee63d94fa9f39965669
Mariana Castro <mariana.castro.sh3@gmail.com>
2025-06-23 14:47:23 -0300

    chore(primevue): correção do tema personalizado de alguns componentes e atualização da biblioteca do primevue

--- src/theme/aura/components/tag.ts
+++ src/theme/aura/components/tag.ts
@@ -1,38 +1,40 @@
 export default {
-  light: {
-    success: {
-      background: "{jade.100}",
-      color: "{jade.700}",
+  colorScheme: {
+    light: {
+      success: {
+        background: "{jade.100}",
+        color: "{jade.700}",
+      },
+      info: {
+        background: "{selenium.100}",
+        color: "{selenium.700}",
+      },
+      warn: {
+        background: "{sulfur.100}",
+        color: "{sulfur.700}",
+      },
+      danger: {
+        background: "{ruby.100}",
+        color: "{ruby.700}",
+      },
     },
-    info: {
-      background: "{selenium.100}",
-      color: "{selenium.700}",
-    },
-    warn: {
-      background: "{sulfur.100}",
-      color: "{sulfur.700}",
-    },
-    danger: {
-      background: "{ruby.100}",
-      color: "{ruby.700}",
-    },
-  },
-  dark: {
-    success: {
-      background: "color-mix(in srgb, {jade.500}, transparent 84%)",
-      color: "{jade.300}",
-    },
-    info: {
-      background: "color-mix(in srgb, {selenium.500}, transparent 84%)",
-      color: "{selenium.300}",
-    },
-    warn: {
-      background: "color-mix(in srgb, {sulfur.500}, transparent 84%)",
-      color: "{sulfur.300}",
-    },
-    danger: {
-      background: "color-mix(in srgb, {ruby.500}, transparent 84%)",
-      color: "{ruby.300}",
+    dark: {
+      success: {
+        background: "color-mix(in srgb, {jade.500}, transparent 84%)",
+        color: "{jade.300}",
+      },
+      info: {
+        background: "color-mix(in srgb, {selenium.500}, transparent 84%)",
+        color: "{selenium.300}",
+      },
+      warn: {
+        background: "color-mix(in srgb, {sulfur.500}, transparent 84%)",
+        color: "{sulfur.300}",
+      },
+      danger: {
+        background: "color-mix(in srgb, {ruby.500}, transparent 84%)",
+        color: "{ruby.300}",
+      },
     },
   },
 };
