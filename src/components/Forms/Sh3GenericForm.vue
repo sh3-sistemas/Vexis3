b26c263b482f60812b06d473d0a26be39cef45b1
Mariana Castro <mariana.castro.sh3@gmail.com>
2025-07-07 17:37:32 -0300

    chore(form): ajuste no estilo geral do ObjectElement e do modo readonly do TextElement

--- src/components/Forms/Sh3GenericForm.vue
+++ src/components/Forms/Sh3GenericForm.vue
@@ -6,7 +6,10 @@
     :endpoint="false"
     :disabled="!options.crud.save"
     :add-classes="{
-      ObjectElement: { container: 'bg-mercury-50 p-3' },
+      ObjectElement: { container: 'bg-mercury-50 my-1.5 p-3 rounded-md' },
+      TextElement: {
+        input: 'read-only:bg-mercury-50 read-only:text-mercury-800',
+      },
       ...(!options.crud.edit ? disableAll : {}),
     }"
     v-bind="$attrs"
