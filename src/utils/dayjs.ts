4da03306da591c3ebb3cd245c21f31b900ba2d1f
guilherme-batista-sh3 <guilherme.evangelista@sh3.com.br>
2025-07-10 13:50:44 -0300

    Feature(package): install e export da biblioteca day.js e seus types

---
+++ src/utils/dayjs.ts
@@ -0,0 +1,16 @@
+import dayjs from "dayjs";
+
+export { dayjs };
+
+export type {
+  ConfigType,
+  OptionType,
+  UnitTypeShort,
+  UnitTypeLong,
+  UnitTypeLongPlural,
+  UnitType,
+  OpUnitType,
+  QUnitType,
+  ManipulateType,
+  PluginFunc,
+} from "dayjs";
